ef5f77ec4fa33712a6daec36c0d71b0fbc9adf29
Jacek Laskowski <jacek@japila.pl>
2017-07-05 09:09:54 +0200

    Use new Trigger in code snippets

--- src/main/scala/KafkaSourceExample.scala
+++ src/main/scala/KafkaSourceExample.scala
@@ -5,22 +5,22 @@
     --property parse.key=true \
     --property key.separator=,
 */
-import org.apache.spark.sql.streaming.ProcessingTime
-import scala.concurrent.duration._
-import org.apache.spark.sql.streaming.OutputMode
-val fromKafkaTopic1ToConsole = spark.readStream
-  .format("kafka")
-  .option("subscribe", "topic1")
-  .option("kafka.bootstrap.servers", "localhost:9092")
-  .option("startingoffsets", "earliest")  // latest, earliest or JSON with {"topicA":{"part":offset,"p1":-1},"topicB":{"0":-2}}
-  .load
-  .select($"key" cast "string", $"value" cast "string") // deserialize records
-  .as[(String, String)]
-  .writeStream
-  .trigger(ProcessingTime(2.seconds))
-  .queryName("from-kafka-to-console")
-  .outputMode(OutputMode.Append)
-  .format("console")
-  .start
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+val fromKafkaTopic1ToConsole = spark.
+  readStream.
+  format("kafka").
+  option("subscribe", "topic1").
+  option("kafka.bootstrap.servers", "localhost:9092").
+  option("startingoffsets", "earliest").  // latest, earliest or JSON with {"topicA":{"part":offset,"p1":-1},"topicB":{"0":-2}}
+  load.
+  select($"key" cast "string", $"value" cast "string"). // deserialize records
+  as[(String, String)].
+  writeStream.
+  trigger(Trigger.ProcessingTime("10 seconds")).
+  queryName("from-kafka-to-console").
+  outputMode(OutputMode.Append).
+  format("console").
+  start
 
+// ...after some time
 fromKafkaTopic1ToConsole.stop
