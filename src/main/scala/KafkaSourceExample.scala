868eb12b67b9ec4f06e706973d9c3d2d899ca651
Jacek Laskowski <jacek@japila.pl>
2019-03-11 11:12:45 +0100

    KafkaMicroBatchReader and Kafka Data Source Options

--- src/main/scala/KafkaSourceExample.scala
+++ src/main/scala/KafkaSourceExample.scala
@@ -12,7 +12,7 @@ val fromKafkaTopic1ToConsole = spark.
   format("kafka").
   option("subscribe", "topic1").
   option("kafka.bootstrap.servers", "localhost:9092").
-  option("startingoffsets", "earliest").  // latest, earliest or JSON with {"topicA":{"part":offset,"p1":-1},"topicB":{"0":-2}}
+  option("startingOffsets", "earliest").  // latest, earliest or JSON with {"topicA":{"part":offset,"p1":-1},"topicB":{"0":-2}}
   load.
   select($"key" cast "string", $"value" cast "string"). // deserialize records
   as[(String, String)].
